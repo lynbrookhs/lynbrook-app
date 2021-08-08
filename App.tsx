e8343446cadec00847a9feed132f988e0aa83498
Oliver Ni <oliver.ni@gmail.com>
2021-08-07 21:08:57 -0700

    Configure OTA updates

--- App.tsx
+++ App.tsx
@@ -1,17 +1,49 @@
 import { StatusBar } from "expo-status-bar";
-import React from "react";
+import { checkForUpdateAsync, fetchUpdateAsync, reloadAsync } from "expo-updates";
+import React, { useEffect, useRef, useState } from "react";
+import { Alert, AppState, AppStateStatus } from "react-native";
 import "react-native-gesture-handler";
 import { SafeAreaProvider } from "react-native-safe-area-context";
 import AuthProvider from "./components/AuthProvider";
 import useCachedResources from "./helpers/useCachedResources";
 import Navigation from "./navigation";
 
+const isActive = (x: AppStateStatus) => x === "active";
+
 const App = () => {
   const isLoadingComplete = useCachedResources();
+  const appState = useRef(AppState.currentState);
+  const [active, setActive] = useState(appState.current === "active");
 
-  if (!isLoadingComplete) {
-    return null;
-  }
+  const checkUpdate = async () => {
+    try {
+      const check = await checkForUpdateAsync();
+      if (!check.isAvailable) return;
+      const update = await fetchUpdateAsync();
+      if (!update.isNew) return;
+    } catch {
+      return;
+      // In development mode or cannot communicate.
+    }
+
+    Alert.alert("Update Available", "The app has been updated and will now reload.", [
+      { text: "OK", onPress: reloadAsync },
+    ]);
+  };
+
+  const handleAppStateChange = (newState: AppStateStatus) => {
+    if (!isActive(appState.current) && isActive(newState)) checkUpdate();
+    appState.current = newState;
+    setActive(newState === "active");
+  };
+
+  useEffect(() => {
+    checkUpdate();
+    AppState.addEventListener("change", handleAppStateChange);
+    return () => AppState.removeEventListener("change", handleAppStateChange);
+  }, []);
+
+  if (!isLoadingComplete) return null;
 
   return (
     <SafeAreaProvider>
