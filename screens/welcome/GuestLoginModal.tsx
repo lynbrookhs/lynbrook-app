7e5ea0c146c7290561d4c64df3c43395c03f675c
Oliver Ni <oliver.ni@gmail.com>
2021-08-09 18:55:17 -0700

    Fix scrolling issues

--- screens/welcome/GuestLoginModal.tsx
+++ screens/welcome/GuestLoginModal.tsx
@@ -1,6 +1,15 @@
 import { Formik } from "formik";
 import React, { useCallback, useState } from "react";
-import { Button, Keyboard, KeyboardAvoidingView, Pressable, Text, TextInput } from "react-native";
+import {
+  Button,
+  Keyboard,
+  KeyboardAvoidingView,
+  Platform,
+  Pressable,
+  ScrollView,
+  Text,
+  TextInput,
+} from "react-native";
 import tw from "tailwind-react-native-classnames";
 import APIError from "../../components/APIError";
 import { useAuth } from "../../components/AuthProvider";
@@ -22,57 +31,59 @@ const GuestLoginModal = ({ navigation }: WelcomeScreenProps) => {
   }, []);
 
   return (
-    <KeyboardAvoidingView behavior="padding" style={tw`flex-1 px-8`}>
-      <Pressable onPress={Keyboard.dismiss} style={tw`flex-1 justify-center`}>
-        <Formik initialValues={{ email: "", password: "" }} onSubmit={handleSignIn}>
-          {({ handleChange, handleBlur, handleSubmit, values }) => (
-            <Stack spacing={8}>
-              <Text style={tw`text-3xl font-bold text-center`}>Guest Login</Text>
+    <KeyboardAvoidingView behavior={Platform.OS == "ios" ? "padding" : "height"} style={tw`flex-1`}>
+      <ScrollView>
+        <Pressable onPress={Keyboard.dismiss} style={tw`flex-1 justify-center p-8`}>
+          <Formik initialValues={{ email: "", password: "" }} onSubmit={handleSignIn}>
+            {({ handleChange, handleBlur, handleSubmit, values }) => (
+              <Stack spacing={8}>
+                <Text style={tw`text-3xl font-bold text-center`}>Guest Login</Text>
 
-              {error && <APIError error={error} style={tw`m-0`} />}
+                {error && <APIError error={error} style={tw`m-0`} />}
 
-              <Stack spacing={4}>
-                <Stack spacing={1}>
-                  <Text style={tw`text-sm font-medium text-gray-700`}>Email Address</Text>
-                  <TextInput
-                    onChangeText={handleChange("email")}
-                    onBlur={handleBlur("email")}
-                    value={values.email}
-                    placeholder="Enter email address"
-                    keyboardType="email-address"
-                    autoCompleteType="email"
-                    textContentType="emailAddress"
-                    style={tw`bg-white py-2 px-4 border border-gray-300 rounded-md`}
+                <Stack spacing={4}>
+                  <Stack spacing={1}>
+                    <Text style={tw`text-sm font-medium text-gray-700`}>Email Address</Text>
+                    <TextInput
+                      onChangeText={handleChange("email")}
+                      onBlur={handleBlur("email")}
+                      value={values.email}
+                      placeholder="Enter email address"
+                      keyboardType="email-address"
+                      autoCompleteType="email"
+                      textContentType="emailAddress"
+                      style={tw`bg-white py-2 px-4 border border-gray-300 rounded-md`}
+                    />
+                  </Stack>
+
+                  <Stack spacing={1}>
+                    <Text style={tw`text-sm font-medium text-gray-700`}>Password</Text>
+                    <TextInput
+                      onChangeText={handleChange("password")}
+                      onBlur={handleBlur("password")}
+                      value={values.password}
+                      placeholder="Enter password"
+                      autoCompleteType="password"
+                      textContentType="password"
+                      secureTextEntry={true}
+                      style={tw`bg-white py-2 px-4 border border-gray-300 rounded-md`}
+                    />
+                  </Stack>
+
+                  <Button title="Sign In" onPress={() => handleSubmit()} />
+
+                  <Divider />
+
+                  <Button
+                    title="Don't have an account?"
+                    onPress={() => navigation.replace("GuestRegister")}
                   />
                 </Stack>
-
-                <Stack spacing={1}>
-                  <Text style={tw`text-sm font-medium text-gray-700`}>Password</Text>
-                  <TextInput
-                    onChangeText={handleChange("password")}
-                    onBlur={handleBlur("password")}
-                    value={values.password}
-                    placeholder="Enter password"
-                    autoCompleteType="password"
-                    textContentType="password"
-                    secureTextEntry={true}
-                    style={tw`bg-white py-2 px-4 border border-gray-300 rounded-md`}
-                  />
-                </Stack>
-
-                <Button title="Sign In" onPress={() => handleSubmit()} />
-
-                <Divider />
-
-                <Button
-                  title="Don't have an account?"
-                  onPress={() => navigation.replace("GuestRegister")}
-                />
               </Stack>
-            </Stack>
-          )}
-        </Formik>
-      </Pressable>
+            )}
+          </Formik>
+        </Pressable>
+      </ScrollView>
     </KeyboardAvoidingView>
   );
 };
--- screens/welcome/GuestRegisterModal.tsx
+++ screens/welcome/GuestRegisterModal.tsx
@@ -1,6 +1,15 @@
 import { Formik } from "formik";
 import React, { useCallback, useState } from "react";
-import { Button, Keyboard, KeyboardAvoidingView, Pressable, Text, TextInput } from "react-native";
+import {
+  Button,
+  Keyboard,
+  KeyboardAvoidingView,
+  Platform,
+  Pressable,
+  ScrollView,
+  Text,
+  TextInput,
+} from "react-native";
 import tw from "tailwind-react-native-classnames";
 import APIError from "../../components/APIError";
 import { useAuth } from "../../components/AuthProvider";
@@ -22,74 +31,76 @@ const GuestRegisterModal = ({ navigation }: WelcomeScreenProps) => {
   }, []);
 
   return (
-    <KeyboardAvoidingView behavior="padding" style={tw`flex-1 px-8`}>
-      <Pressable onPress={Keyboard.dismiss} style={tw`flex-1 justify-center`}>
-        <Formik
-          initialValues={{ email: "", password: "", re_password: "" }}
-          onSubmit={handleRegister}
-        >
-          {({ handleChange, handleBlur, handleSubmit, values }) => (
-            <Stack spacing={8}>
-              <Text style={tw`text-3xl font-bold text-center`}>Guest Registration</Text>
+    <KeyboardAvoidingView behavior={Platform.OS == "ios" ? "padding" : "height"} style={tw`flex-1`}>
+      <ScrollView>
+        <Pressable onPress={Keyboard.dismiss} style={tw`flex-1 justify-center p-8`}>
+          <Formik
+            initialValues={{ email: "", password: "", re_password: "" }}
+            onSubmit={handleRegister}
+          >
+            {({ handleChange, handleBlur, handleSubmit, values }) => (
+              <Stack spacing={8}>
+                <Text style={tw`text-3xl font-bold text-center`}>Guest Registration</Text>
 
-              {error && <APIError error={error} style={tw`m-0`} />}
+                {error && <APIError error={error} style={tw`m-0`} />}
 
-              <Stack spacing={4}>
-                <Stack spacing={1}>
-                  <Text style={tw`text-sm font-medium text-gray-700`}>Email Address</Text>
-                  <TextInput
-                    onChangeText={handleChange("email")}
-                    onBlur={handleBlur("email")}
-                    value={values.email}
-                    placeholder="Enter email address"
-                    keyboardType="email-address"
-                    autoCompleteType="email"
-                    textContentType="emailAddress"
-                    style={tw`bg-white py-2 px-4 border border-gray-300 rounded-md`}
+                <Stack spacing={4}>
+                  <Stack spacing={1}>
+                    <Text style={tw`text-sm font-medium text-gray-700`}>Email Address</Text>
+                    <TextInput
+                      onChangeText={handleChange("email")}
+                      onBlur={handleBlur("email")}
+                      value={values.email}
+                      placeholder="Enter email address"
+                      keyboardType="email-address"
+                      autoCompleteType="email"
+                      textContentType="emailAddress"
+                      style={tw`bg-white py-2 px-4 border border-gray-300 rounded-md`}
+                    />
+                  </Stack>
+
+                  <Stack spacing={1}>
+                    <Text style={tw`text-sm font-medium text-gray-700`}>Create Password</Text>
+                    <TextInput
+                      onChangeText={handleChange("password")}
+                      onBlur={handleBlur("password")}
+                      value={values.password}
+                      placeholder="Create password"
+                      autoCompleteType="password"
+                      textContentType="newPassword"
+                      secureTextEntry={true}
+                      style={tw`bg-white py-2 px-4 border border-gray-300 rounded-md`}
+                    />
+                  </Stack>
+
+                  <Stack spacing={1}>
+                    <Text style={tw`text-sm font-medium text-gray-700`}>Verify Password</Text>
+                    <TextInput
+                      onChangeText={handleChange("re_password")}
+                      onBlur={handleBlur("re_password")}
+                      value={values.re_password}
+                      placeholder="Repeat password"
+                      autoCompleteType="password"
+                      textContentType="password"
+                      secureTextEntry={true}
+                      style={tw`bg-white py-2 px-4 border border-gray-300 rounded-md`}
+                    />
+                  </Stack>
+
+                  <Button title="Register" onPress={() => handleSubmit()} />
+
+                  <Divider />
+
+                  <Button
+                    title="Already have an account?"
+                    onPress={() => navigation.replace("GuestLogin")}
                   />
                 </Stack>
-
-                <Stack spacing={1}>
-                  <Text style={tw`text-sm font-medium text-gray-700`}>Create Password</Text>
-                  <TextInput
-                    onChangeText={handleChange("password")}
-                    onBlur={handleBlur("password")}
-                    value={values.password}
-                    placeholder="Create password"
-                    autoCompleteType="password"
-                    textContentType="newPassword"
-                    secureTextEntry={true}
-                    style={tw`bg-white py-2 px-4 border border-gray-300 rounded-md`}
-                  />
-                </Stack>
-
-                <Stack spacing={1}>
-                  <Text style={tw`text-sm font-medium text-gray-700`}>Verify Password</Text>
-                  <TextInput
-                    onChangeText={handleChange("re_password")}
-                    onBlur={handleBlur("re_password")}
-                    value={values.re_password}
-                    placeholder="Repeat password"
-                    autoCompleteType="password"
-                    textContentType="password"
-                    secureTextEntry={true}
-                    style={tw`bg-white py-2 px-4 border border-gray-300 rounded-md`}
-                  />
-                </Stack>
-
-                <Button title="Register" onPress={() => handleSubmit()} />
-
-                <Divider />
-
-                <Button
-                  title="Already have an account?"
-                  onPress={() => navigation.replace("GuestLogin")}
-                />
               </Stack>
-            </Stack>
-          )}
-        </Formik>
-      </Pressable>
+            )}
+          </Formik>
+        </Pressable>
+      </ScrollView>
     </KeyboardAvoidingView>
   );
 };
