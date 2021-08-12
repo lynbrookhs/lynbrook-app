0c29a0547bedebf4b02d4b39869656730bbe5375
Oliver Ni <oliver.ni@gmail.com>
2021-08-12 14:51:30 -0700

    Improve rewards screen

--- screens/home/RewardsScreen.tsx
+++ screens/home/RewardsScreen.tsx
@@ -1,38 +1,36 @@
-import { Ionicons } from "@expo/vector-icons";
-import React, { useLayoutEffect } from "react";
-import { ActivityIndicator, ScrollView, Text, TouchableOpacity, View } from "react-native";
-import AutoHeightImage from "react-native-auto-height-image";
-import ProgressCircle from "react-native-progress-circle";
+import React from "react";
+import { ActivityIndicator, ScrollView, Text } from "react-native";
 import tw from "tailwind-react-native-classnames";
 import APIError from "../../components/APIError";
 import Card from "../../components/Card";
-import Stack from "../../components/Stack";
 import FilledButton from "../../components/FilledButton";
+import Stack from "../../components/Stack";
 import { usePrizes, useUser } from "../../helpers/api";
-import { OrganizationType } from "../../helpers/api/models";
+import { NestedMembership, Prize } from "../../helpers/api/models";
 import { RewardsScreenProps } from "../../navigation/tabs/HomeNavigator";
 
 type RewardItemProps = {
-  name: string;
-  description: string;
-  points: number;
-  organization: string;
+  prize: Prize;
+  canClaim: boolean;
 };
 
-const RewardItem = ({ name, description, points, organization }: RewardItemProps) => (
-  <Card key={name}>
-    <Stack direction="row" style={tw`justify-between`} align="center">
-      <Stack spacing={2}>
-        <Stack>
-          <Text style={tw`text-lg font-bold`}>{name}</Text>
-          <Text style={tw`text-sm`}>{description}</Text>
+const RewardItem = ({ prize, canClaim }: RewardItemProps) => (
+  <Card
+    header={
+      <Stack direction="row" align="center">
+        <Stack style={tw`flex-1`}>
+          <Text style={tw`text-lg font-bold`}>{prize.name}</Text>
+          <Text style={tw`text-sm`}>{prize.organization.name}</Text>
         </Stack>
-        <Stack direction="row" spacing={2} align="center">
-          <Text style={tw`text-4xl font-bold`}>{points}</Text>
-          <Text style={tw`text-2xl`}>{organization} Points</Text>
-        </Stack>
+        <Text style={tw`text-base`}>{prize.points} points</Text>
       </Stack>
-      <FilledButton>Claim</FilledButton>
+    }
+  >
+    <Stack spacing={4}>
+      <Text style={tw`text-sm`}>{prize.description}</Text>
+      <FilledButton textStyle={tw`text-center`} disabled={!canClaim}>
+        {canClaim ? "Claim" : "Not Enough Points"}
+      </FilledButton>
     </Stack>
   </Card>
 );
@@ -46,25 +44,19 @@ const RewardsScreen = ({ navigation }: RewardsScreenProps) => {
   if (!user) return <ActivityIndicator style={tw`m-4`} />;
   if (!prizes) return <ActivityIndicator style={tw`m-4`} />;
 
-  //   const asb = user.memberships.find((x) => x.organization.id === 2);
-  //   const cls = user.memberships.find((x) => x.organization.type === OrganizationType.CLASS);
-
-  //   const nextAsbPrize = prizes.find(
-  //     (x) => x.organization.id === asb?.organization.id && x.points > asb.points
-  //   );
-  //   const nextClsPrize = prizes.find(
-  //     (x) => x.organization.id === cls?.organization.id && x.points > cls.points
-  //   );
+  const membershipsByOrg = user.memberships.reduce<{ [key: number]: NestedMembership }>(
+    (acc, val) => ({ ...acc, [val.organization.id]: val }),
+    {}
+  );
 
   return (
     <ScrollView style={tw`flex-1`}>
       <Stack spacing={4} style={tw`p-4`}>
         {prizes.map((item) => (
           <RewardItem
-            name={item.name}
-            description={item.description}
-            points={item.points}
-            organization={"ASB"}
+            key={item.id}
+            prize={item}
+            canClaim={membershipsByOrg[item.organization.id].points >= item.points}
           />
         ))}
       </Stack>
--- screens/home/SpecialEventsScreen.tsx
+++ screens/home/SpecialEventsScreen.tsx
@@ -1,36 +1,11 @@
-import { Ionicons } from "@expo/vector-icons";
+import { format, parseISO } from "date-fns";
 import React, { useEffect } from "react";
-import { parseISO, format } from "date-fns";
-import { ActivityIndicator, EventEmitter, ScrollView, Text, TouchableOpacity, View } from "react-native";
-import AutoHeightImage from "react-native-auto-height-image";
-import ProgressCircle from "react-native-progress-circle";
+import { ActivityIndicator, ScrollView, Text } from "react-native";
 import tw from "tailwind-react-native-classnames";
 import APIError from "../../components/APIError";
-import Card from "../../components/Card";
 import Stack from "../../components/Stack";
 import { useEvent, useUser } from "../../helpers/api";
-import { OrganizationType } from "../../helpers/api/models";
 import { SpecialEventsScreenProps } from "../../navigation/tabs/HomeNavigator";
-
-type RewardItemProps = {
-  name: string;
-  description: string;
-  points: number;
-  organization: string;
-};
-
-const RewardItem = ({ name, description, points, organization }: RewardItemProps) => (
-  <Card header={name && <Stack><Text style={tw`text-2xl font-bold`}>{name}</Text><Text style={tw`text-xl`}>{description}</Text></Stack>} key={name}>
-    <Stack direction="row" spacing={6} align="center">
-      <Stack style={tw`flex-1`}>
-        <Stack direction="row" spacing={2} align="center">
-          <Text style={tw`text-4xl font-bold`}>{points}</Text>
-          <Text style={tw`text-2xl`}>{organization} Points</Text>
-        </Stack>
-      </Stack>
-    </Stack>
-  </Card>
-);
 
 const SpecialEventsScreen = ({ navigation, route }: SpecialEventsScreenProps) => {
   const { data: user, error } = useUser();
@@ -51,8 +26,10 @@ const SpecialEventsScreen = ({ navigation, route }: SpecialEventsScreenProps) =>
     <ScrollView style={tw`flex-1`}>
       <Stack spacing={4} style={tw`p-4`}>
         <Stack spacing={1}>
-            <Text style={tw`text-3xl font-bold`}>{event.name}</Text>
-            <Text style={tw`text-sm`}>{format(parseISO(event.start), "MM/dd")} - {format(parseISO(event.end), "MM/dd")}</Text>
+          <Text style={tw`text-3xl font-bold`}>{event.name}</Text>
+          <Text style={tw`text-sm`}>
+            {format(parseISO(event.start), "M/dd")} - {format(parseISO(event.end), "M/dd")}
+          </Text>
         </Stack>
         <Text style={tw`text-base`}>{event.description}</Text>
       </Stack>
