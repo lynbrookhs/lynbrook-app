76ca44ac4fbc71ffb843c96419616fc4448f689e
Oliver Ni <oliver.ni@gmail.com>
2021-08-12 01:42:36 -0700

    Add button component

---
+++ components/FilledButton.tsx
@@ -0,0 +1,27 @@
+import React from "react";
+import { Pressable, PressableProps, StyleProp, Text, TextStyle, View } from "react-native";
+import tw from "tailwind-react-native-classnames";
+
+export type FilledButton = PressableProps & {
+  color?: string;
+  textStyle?: StyleProp<TextStyle>;
+};
+
+const FilledButton = ({ color = "indigo", textStyle, children, ...props }: FilledButton) => {
+  return (
+    <Pressable {...props}>
+      {({ pressed }) => (
+        <View
+          style={[
+            tw`bg-${color}-600 px-3 py-2 border border-transparent rounded shadow-md`,
+            pressed && tw`bg-${color}-700`,
+          ]}
+        >
+          <Text style={[tw`text-white text-base font-medium`, textStyle]}>{children}</Text>
+        </View>
+      )}
+    </Pressable>
+  );
+};
+
+export default FilledButton;
