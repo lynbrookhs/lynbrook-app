35c98e9ae625dd179c46e2542c2d9f65cbe6cf68
Joe Lin <lin0joe24@gmail.com>
2021-08-22 23:10:26 -0700

    Fixed Live Points

--- screens/home/SpecialEventsScreen.tsx
+++ screens/home/SpecialEventsScreen.tsx
@@ -14,7 +14,9 @@ const CLASSES = ["2022", "2023", "2024", "2025"];
 
 const SpecialEventsScreen = ({ navigation, route }: SpecialEventsScreenProps) => {
   const { data: user, error } = useUser();
+
   const { data: event, error: error2 } = useEvent(route.params.id);
+  const { data: event2, error: error3 } = useEvent(10);
 
   useEffect(() => {
     if (event) {
@@ -24,8 +26,10 @@ const SpecialEventsScreen = ({ navigation, route }: SpecialEventsScreenProps) =>
 
   if (error) return <APIError error={error} />;
   if (error2) return <APIError error={error2} />;
+  if (error3) return <APIError error={error3} />;
   if (!user) return <ActivityIndicator style={tw`m-4`} />;
   if (!event) return <ActivityIndicator style={tw`m-4`} />;
+  if (!event2) return <ActivityIndicator style={tw`m-4`} />;
 
   return (
     <Stack style={tw`flex-1`}>
@@ -40,7 +44,7 @@ const SpecialEventsScreen = ({ navigation, route }: SpecialEventsScreenProps) =>
           <Stack spacing={2}>
             <Text style={tw`text-lg font-bold`}>Leaderboard</Text>
             {CLASSES.map((c, idx) => (
-              <Text key={idx} style={tw`text-lg`}>{c}: {event.leaderboard[c] ? event.leaderboard[c] * event.points : 0}</Text>
+              <Text key={idx} style={tw`text-lg`}>{c}: {event2.leaderboard[c] ? event2.leaderboard[c] * event2.points : 0}</Text>
             ))}
             <Text style={tw`text-lg font-bold`}>Event Description</Text>
             <Markdown style={markdownStyles}>{event.description}</Markdown>
