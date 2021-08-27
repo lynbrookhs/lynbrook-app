4e2cf70c1d69b0174ee5af92497ff23ff397597b
Oliver Ni <oliver.ni@gmail.com>
2021-08-27 02:44:53 -0700

    Add points to clubs screen

--- screens/activities/ClubsScreen.tsx
+++ screens/activities/ClubsScreen.tsx
@@ -1,13 +1,14 @@
 import { Ionicons } from "@expo/vector-icons";
 import {
   ClubCategory,
+  Membership,
   Organization,
   OrganizationType,
   useMemberships,
   useOrgs,
   useRequest,
 } from "lynbrook-app-api-hooks";
-import React, { useCallback, useLayoutEffect, useState } from "react";
+import React, { PropsWithChildren, useCallback, useLayoutEffect, useState } from "react";
 import { SectionList, Text, TouchableHighlight, TouchableOpacity } from "react-native";
 import tw from "tailwind-react-native-classnames";
 
@@ -20,33 +21,52 @@ import { ClubsScreenProps } from "../../navigation/tabs/ActivitiesNavigator";
 
 type ClubItemProps = {
   item: Organization;
-  subscribed: boolean;
+  points?: number;
   onAdd: () => void;
   onRemove: () => void;
   onPress: () => void;
 };
 
-const ClubItem = ({ item, subscribed, onAdd, onRemove, onPress }: ClubItemProps) => (
-  <TouchableHighlight onPress={onPress}>
-    <ListItem text={item.name} direction="row" align="center">
-      {item.type === OrganizationType.CLUB && (
-        <Stack direction="row" spacing={2} align="center">
-          <TouchableOpacity onPress={subscribed ? onRemove : onAdd}>
-            <Ionicons
-              name={subscribed ? "remove-circle-outline" : "add-circle-outline"}
-              style={tw`text-${subscribed ? "red" : "green"}-500 text-2xl`}
-            />
-          </TouchableOpacity>
-          <Ionicons name="chevron-forward" style={tw`text-lg text-gray-500`} />
+const ClubItem = ({ item, points, onAdd, onRemove, onPress }: ClubItemProps) => {
+  const subscribed = points !== undefined;
+
+  const Wrapper = ({ children }: PropsWithChildren<object>) =>
+    item.type === OrganizationType.CLUB ? (
+      <TouchableHighlight onPress={onPress}>{children}</TouchableHighlight>
+    ) : (
+      <>{children}</>
+    );
+
+  return (
+    <Wrapper>
+      <ListItem direction="row" align="center">
+        <Stack direction="row" style={tw`flex-1`} align="center" spacing={4}>
+          <Text style={tw`text-base`}>{item.name}</Text>
+          {points !== undefined && points > 0 && (
+            <Text style={tw`text-gray-500 text-sm`}>{points} points</Text>
+          )}
         </Stack>
-      )}
-    </ListItem>
-  </TouchableHighlight>
-);
+
+        {item.type === OrganizationType.CLUB && (
+          <Stack direction="row" spacing={4} align="center">
+            <TouchableOpacity onPress={subscribed ? onRemove : onAdd}>
+              <Ionicons
+                name={subscribed ? "person-remove-outline" : "person-add-outline"}
+                style={tw`text-${subscribed ? "red" : "green"}-500 text-lg`}
+              />
+            </TouchableOpacity>
+
+            <Ionicons name="chevron-forward" style={tw`text-lg text-gray-500`} />
+          </Stack>
+        )}
+      </ListItem>
+    </Wrapper>
+  );
+};
 
 const ClubsScreen = ({ navigation }: ClubsScreenProps) => {
   const { data: memberships, error, mutate } = useMemberships();
-  const { data: orgs, error: error2 } = useOrgs();
+  const { data: _orgs, error: error2 } = useOrgs();
   const { request, error: error3 } = useRequest();
   const [sorted, setSorted] = useState(false);
 
@@ -83,16 +103,20 @@ const ClubsScreen = ({ navigation }: ClubsScreenProps) => {
   if (error2) return <APIError error={error2} />;
   if (error3) return <APIError error={error3} />;
   if (!memberships) return <Loading />;
-  if (!orgs) return <Loading />;
+  if (!_orgs) return <Loading />;
 
-  const clubs = orgs.filter((x) => x.type === OrganizationType.CLUB);
+  const membershipsById = memberships.reduce((acc, val) => {
+    acc.set(val.organization.id, val);
+    return acc;
+  }, new Map<number, Membership>());
 
-  const userOrgIds = memberships.map((x) => x.organization.id);
+  const orgs = _orgs.map((x) => ({ ...x, membership: membershipsById.get(x.id) }));
+
   const userOrgs = [
-    ...orgs.filter((x) => x.type !== OrganizationType.CLUB && userOrgIds.includes(x.id)),
-    ...clubs.filter((x) => userOrgIds.includes(x.id)),
+    ...orgs.filter((x) => x.type !== OrganizationType.CLUB && x.membership),
+    ...orgs.filter((x) => x.type === OrganizationType.CLUB && x.membership),
   ];
-  const otherClubs = clubs.filter((x) => !userOrgIds.includes(x.id));
+  const otherClubs = orgs.filter((x) => !x.membership);
 
   const listData = sorted
     ? [
@@ -110,13 +134,13 @@ const ClubsScreen = ({ navigation }: ClubsScreenProps) => {
       ];
 
   return (
-    <SectionList<Organization>
+    <SectionList<typeof orgs[0]>
       style={tw`-mb-px`}
       sections={listData}
       renderItem={({ item }) => (
         <ClubItem
           item={item}
-          subscribed={userOrgs.includes(item)}
+          points={item.membership?.points}
           onAdd={() => handleAdd(item)}
           onRemove={() => handleRemove(item)}
           onPress={() => navigation.navigate("ClubDetail", { id: item.id })}
