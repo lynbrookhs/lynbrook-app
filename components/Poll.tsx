75cecc2236f9003851f34d1e4a6fcfe576adfd2b
Oliver Ni <oliver.ni@gmail.com>
2022-01-23 22:05:21 -0800

    Finish polls

--- components/Poll.tsx
+++ components/Poll.tsx
@@ -1,89 +1,116 @@
 import { Ionicons } from "@expo/vector-icons";
+import { Poll as Poll_, PollSubmission, PollType, useRequest } from "lynbrook-app-api-hooks";
 import React, { useState } from "react";
-import { Pressable, Text, View } from "react-native";
+import { Pressable, Text } from "react-native";
+import { mutate } from "swr";
 import tw from "tailwind-react-native-classnames";
 
+import APIError from "./APIError";
 import Card from "./Card";
 import FilledButton from "./FilledButton";
 import Stack from "./Stack";
 
-export type PollProps = {
-  name: string;
-  description: string;
-  choices: string[];
-  min: number;
-  max: number;
+type PollChoiceProps = {
+  text: string;
+  disabled: boolean;
+  selected: boolean;
+  onChange: (selected: boolean) => void;
 };
 
-const SelectPoll = ({ name, description, choices, min, max }: PollProps) => {
-  const [selected, setSelected] = useState(new Array(choices.length).fill(false));
-  const [disabled, setDisabled] = useState(false);
-  const [submit, setSubmit] = useState(false);
+const PollChoice = ({ text, disabled, selected, onChange }: PollChoiceProps) => {
   return (
-    <Card
-      header={
-        <Stack direction="row" align="center">
-          <Stack style={tw`flex-1`}>
-            <Text style={tw`text-lg font-bold`}>{name}</Text>
-            <Text style={tw`text-sm`}>{description}</Text>
-          </Stack>
-          <FilledButton style={tw`text-base`} disabled={!submit}>
-            Submit
-          </FilledButton>
+    <Pressable onPress={() => onChange(!selected)} disabled={disabled}>
+      {({ pressed }) => (
+        <Stack
+          style={[
+            tw`px-3 py-2 border border-transparent rounded shadow-md`,
+            selected ? tw`bg-indigo-600` : tw`bg-white`,
+            pressed && (selected ? tw`bg-indigo-700` : tw`bg-gray-50`),
+            disabled && tw`opacity-50`,
+          ]}
+          direction="row"
+          align="center"
+          spacing={2}
+        >
+          <Ionicons
+            name={selected ? "checkmark-circle" : "radio-button-off"}
+            color={selected ? "white" : "black"}
+            style={tw`text-base`}
+          />
+          <Text style={[tw`text-base`, selected ? tw`text-white` : tw`text-black`]}>{text}</Text>
         </Stack>
-      }
-    >
-      <Stack direction="col" align="center" spacing={2}>
-        {choices.map((c, i) => {
-          return (
-            <Pressable
-              key={i}
-              onPress={() => {
-                selected[i] = !selected[i];
-                setSelected(selected);
-                if (selected.filter(Boolean).length >= max) {
-                  setDisabled(true);
-                } else {
-                  setDisabled(false);
+      )}
+    </Pressable>
+  );
+};
+
+export type PollProps = {
+  poll: Poll_;
+  submission?: PollSubmission;
+};
+
+const Poll = ({ poll, submission }: PollProps) => {
+  const [selected, setSelected] = useState<string[]>(submission?.responses ?? []);
+  const { request, error } = useRequest();
+
+  if (poll.type !== PollType.SELECT) {
+    throw new Error("Only select polls are implemented.");
+  }
+
+  const handleSubmit = async () => {
+    await request("POST", `/posts/${poll.post}/polls/${poll.id}/submissions/`, {
+      poll: poll.id,
+      responses: selected,
+    });
+    mutate(`/posts/${poll.post}/polls/`);
+    mutate(`/posts/${poll.post}/polls/${poll.id}/`);
+    mutate(`/posts/${poll.post}/polls/${poll.id}/submissions/`);
+  };
+
+  return (
+    <Card header={<Text style={tw`text-lg font-bold`}>{poll.description}</Text>}>
+      <Stack spacing={4}>
+        {error && <APIError error={error} style={tw`m-0`} />}
+
+        <Stack spacing={2}>
+          {poll.choices.map((choice) => {
+            return (
+              <PollChoice
+                key={choice}
+                text={choice}
+                disabled={
+                  submission !== undefined ||
+                  (selected.length >= poll.max_values && !selected.includes(choice))
                 }
-                if (selected.filter(Boolean).length < min) {
-                  setSubmit(false);
-                } else {
-                  setSubmit(true);
-                }
-              }}
-              disabled={disabled && !selected[i]}
-              style={tw`w-11/12`}
-            >
-              {({ pressed }) => (
-                <Stack
-                  style={[
-                    tw`justify-center px-3 py-2 border border-transparent rounded shadow-md`,
-                    selected[i] ? tw`bg-indigo-600` : tw`bg-white`,
-                    pressed && (selected[i] ? tw`bg-indigo-700` : tw`bg-white`),
-                    disabled && !selected[i] && tw`opacity-50`,
-                  ]}
-                >
-                  <View style={tw`flex-row items-center justify-start`}>
-                    <Ionicons
-                      name={selected[i] ? "checkmark-circle" : "radio-button-off"}
-                      color={selected[i] ? "white" : "black"}
-                      style={tw`text-sm`}
-                    />
-                    <Text
-                      style={[tw`text-base pl-2`, selected[i] ? tw`text-white` : tw`text-black`]}
-                    >
-                      {c}
-                    </Text>
-                  </View>
-                </Stack>
-              )}
-            </Pressable>
-          );
-        })}
+                selected={selected.includes(choice)}
+                onChange={(state) => {
+                  if (state) {
+                    setSelected([...selected, choice]);
+                  } else {
+                    const newSelected = [...selected];
+                    newSelected.splice(newSelected.indexOf(choice), 1);
+                    setSelected(newSelected);
+                  }
+                }}
+              />
+            );
+          })}
+        </Stack>
+
+        {submission === undefined && (
+          <FilledButton
+            textStyle={tw`text-center`}
+            disabled={selected.length < poll.min_values || selected.length > poll.max_values}
+            onPress={handleSubmit}
+          >
+            {selected.length >= poll.min_values
+              ? "Submit"
+              : `Select ${poll.min_values - selected.length} more`}
+          </FilledButton>
+        )}
       </Stack>
     </Card>
   );
 };
 
-export default SelectPoll;
+export default Poll;
--- screens/news/PostDetailScreen.tsx
+++ screens/news/PostDetailScreen.tsx
@@ -1,18 +1,20 @@
 import { format } from "date-fns";
-import { usePost } from "lynbrook-app-api-hooks";
+import { usePolls, usePost } from "lynbrook-app-api-hooks";
 import React, { useEffect } from "react";
-import { ScrollView, Text, View } from "react-native";
+import { ScrollView, Text } from "react-native";
 import Markdown from "react-native-markdown-display";
 import tw from "tailwind-react-native-classnames";
 
 import APIError from "../../components/APIError";
 import Loading from "../../components/Loading";
+import Poll from "../../components/Poll";
 import Stack from "../../components/Stack";
 import markdownStyles from "../../helpers/markdownStyles";
 import { PostDetailScreenProps } from "../../navigation/tabs/NewsNavigator";
 
 const PostDetailScreen = ({ navigation, route }: PostDetailScreenProps) => {
   const { data: post, error } = usePost(route.params.id);
+  const { data: polls, error: error2 } = usePolls(route.params.id);
 
   useEffect(() => {
     if (post) {
@@ -21,6 +23,7 @@ const PostDetailScreen = ({ navigation, route }: PostDetailScreenProps) => {
   }, [post]);
 
   if (error) return <APIError error={error} />;
+  if (error2) return <APIError error={error2} />;
   if (!post) return <Loading />;
 
   return (
@@ -33,9 +36,12 @@ const PostDetailScreen = ({ navigation, route }: PostDetailScreenProps) => {
         </Text>
       </Stack>
       <ScrollView style={tw`bg-white flex-1`}>
-        <View style={tw`p-3`}>
+        <Stack style={tw`p-3`} spacing={2}>
           <Markdown style={markdownStyles}>{post.content}</Markdown>
-        </View>
+          {polls?.map((x) => (
+            <Poll key={x.id} poll={x} submission={x.submissions[0]} />
+          ))}
+        </Stack>
       </ScrollView>
     </Stack>
   );
